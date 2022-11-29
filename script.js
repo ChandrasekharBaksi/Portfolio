ca643dbde17a28fb7c210afc231ed9849485254a
Chandrasekhar Baksi <david_lpu@Chandrasekhars-MacBook-Air.local>
2022-11-29 18:51:08 +0530

    First commit

--- script.js
+++ script.js
@@ -12,3 +12,37 @@ tabsContainer.addEventListener("click", (e) =>{
        aboutSection.querySelector(target).classList.add("active");
     }
 });
+
+/* --------------Portfolio Item Details Popup  ------------> */
+
+document.addEventListener("click", (e) =>{
+    if(e.target.classList.contains("view-project-btn")){
+        togglePortfolioPopup();
+        document.querySelector(".portfolio-popup").scrollTo(0,0);
+        portfolioItemDetails(e.target.parentElement);
+    }
+ })
+ function togglePortfolioPopup(){
+    document.querySelector(".portfolio-popup").classList.toggle("open");
+    document.body.classList.toggle("hide-scrolling");
+    document.querySelector(".main").classList.toggle("fade-out");
+}
+document.querySelector(".pp-close").addEventListener("click", togglePortfolioPopup);
+
+//Hide popup when clicking outside of it
+document.addEventListener("click", (e) =>{
+    if(e.target.classList.contains("pp-inner")){
+        togglePortfolioPopup();
+    }
+})
+
+function portfolioItemDetails(portfolioItem){
+    document.querySelector(".pp-thumbnail img").src =
+    portfolioItem.querySelector(".portfolio-item-thumbnail img").src;
+
+    document.querySelector(".pp-header h3").innerHTML =
+    portfolioItem.querySelector(".portfolio-item-title").innerHTML;
+
+    document.querySelector(".pp-body").innerHTML =
+    portfolioItem.querySelector(".portfolio-item-details").innerHTML;
+}
